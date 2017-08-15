f535573fb587250e30c1c0c3df84771bc70c4c78
Lana Black <lanablack@amok.cc>
2017-08-15 06:30:36 +0000

    Add cabal debug flag.

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,6 @@
 
 rm libpghaskell.so
 stack clean
-stack build --flag pghaskell:-dummy
+#stack build --flag pghaskell:-dummy --flag pghaskell:debug --flag pghaskell-internal:debug
+stack build --flag pghaskell:dummy --flag pghaskell:debug --flag pghaskell-internal:debug
 ln -sf pghaskell/.stack-work/dist/x86_64-linux/Cabal-1.24.2.0/build/libHSpghaskell-*.so libpghaskell.so
