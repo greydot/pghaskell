36a83ad52e772710b9e8f88f197e402eabb2a387
Lana Black <lanablack@amok.cc>
2017-08-15 07:26:41 +0000

    Commit the uncommited.

---
+++ pghaskell-internal/src/PgHaskell/Types.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+module PgHaskell.Types ( PG
+                       , PgValue(..)
+                       , PgTypeName
+                       ) where
+
+import Data.ByteString (ByteString)
+import Data.Proxy (Proxy)
+import Data.Text (Text)
+import Data.Typeable (Typeable)
+
+import Foreign.C.Types (CChar, CLong)
+
+newtype PG a = MkPG { runPG :: IO a }
+
+data PgValue where
+  -- Note, `t' should be constrained
+  MkPgValue :: PgTypeable t => t -> PgValue
+
+class Typeable t => PgTypeable t where
+  type PgType t :: *
+
+  pgTypeName :: Proxy t -> PgTypeName
+  toPgType :: t -> PgType t
+  fromPgType :: PgType t -> t
+
+type PgTypeName = Text
+
+-- ^ Haskell FFI doesn't have anything resembling CBool,
+-- | but since sizeof(bool) == 1 we can be clever here.
+instance PgTypeable Bool where
+  type PgType Bool = CChar
+
+  pgTypeName _ = "boolean"
+  toPgType b = if b then 1 else 0
+  fromPgType 0 = False
+  fromPgType _ = True
+
+instance PgTypeable Int where
+  type PgType Int = CLong
+
+  pgTypeName _ = "bigint"
+  toPgType = fromIntegral
+  fromPgType = fromIntegral
--- pghaskell/src/PgHaskell/Compiler.hs
+++ pghaskell/src/PgHaskell/Compiler.hs
@@ -5,10 +5,11 @@ import PgHaskell.Internal
 
 import Data.Text (Text)
 import qualified Data.Text as Text
+import Data.Typeable
 
 import Language.Haskell.Interpreter
 
-compileFunction :: Text -> IO (Either InterpreterError ([HsValue] -> IO [HsValue]))
+compileFunction :: Text -> IO (Either InterpreterError ([PgValue] -> IO [PgValue]))
 compileFunction txt = runInterpreter $ do
     setImports ["Prelude"
                ,"Data.Monoid"
@@ -20,7 +21,10 @@ compileFunction txt = runInterpreter $ do
 
 test :: IO ()
 test = do
-  r <- compileFunction "\\is -> let p = (pure :: [HsValue] -> IO [HsValue]) in p $ map (id) is"
-  case r of
-    Left e -> print e
-    Right f -> pure (f []) >> putStrLn "OK"
+    r <- compileFunction "\\is -> let p = (pure :: [PgValue] -> IO [PgValue]) in p $ map (id) is"
+    case r of
+      Left e -> print e
+      Right f -> pure (f []) >> print (typeRep $ f [MkPgValue i])
+  where
+    i = 123 :: Int
+
