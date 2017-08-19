9b6626d39935da863c0e9f899dc76df8c70ed42b
Lana Black <lanablack@amok.cc>
2017-08-19 17:28:22 +0000

    Build script.

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 rm libpghaskell.so
-stack clean
-#stack build --flag pghaskell:-dummy --flag pghaskell:debug --flag pghaskell-internal:debug
-stack build --flag pghaskell:dummy --flag pghaskell:debug --flag pghaskell-internal:debug
-ln -sf pghaskell/.stack-work/dist/x86_64-linux/Cabal-1.24.2.0/build/libHSpghaskell-*.so libpghaskell.so
+echo
+echo "Building dev version with stack"
+echo
+stack build --flag pghaskell-internal:debug --flag pghaskell:debug || exit 1
+
+echo
+echo "Buiding test version with cabal"
+echo
+for d in pghaskell-internal pghaskell; do
+    cd $d
+    cabal install -fdebug --force-reinstalls
+    rm -r dist
+    cd ..
+done
+ln -sf ~/.cabal/lib/x86_64-linux-ghc-8.0.2/libHSpghaskell-0.1-*.so libpghaskell.so
