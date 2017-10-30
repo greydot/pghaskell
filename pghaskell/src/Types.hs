f7a8585f6712a93933d4a6bedac4e3d41129147d
Lana Black <lanablack@amok.cc>
2017-10-30 17:11:18 +0000

    Remove this.

--- pghaskell/src/Types.hs
+++
@@ -1,7 +0,0 @@
-module PgHaskell.Types where
-
-import Language.Haskell.Interpreter
-
-data PgProc = PgProc { procCode :: String
-                     , procImports :: [ModuleName]
-                     } deriving (Show)
