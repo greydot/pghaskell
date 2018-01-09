8b9f1f58a72c72272cf7ff6c3bc5961ffb09aa4f
Lana Black <lanablack@amok.cc>
2018-01-09 11:37:01 +0000

    Missing exit code.

--- rebuild.sh
+++ rebuild.sh
@@ -6,6 +6,6 @@ stack clean || exit 1
 
 echo "Building dev version with stack"
 stack install --flag pghaskell-internal:debug --flag pghaskell:debug || exit 1
-ln -s `stack path --local-install-root`/lib/libpghaskell.so . || exit 
+ln -s `stack path --local-install-root`/lib/libpghaskell.so . || exit 1
 echo "Done"
 
