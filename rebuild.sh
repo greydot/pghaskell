ccfd53d792ee16e4d56ad848a10b9e0e44957633
Lana Black <lanablack@amok.cc>
2018-01-10 07:54:46 +0000

    Use cabal builds for testing with postgresql.

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+PATH="$HOME/.local/bin:$PATH"
+
 echo "Cleaning up"
 [ -s libpghaskell.so ] && rm libpghaskell.so
 stack clean || exit 1
 
 echo "Building dev version with stack"
 stack install --flag pghaskell-internal:debug --flag pghaskell:debug || exit 1
-ln -s `stack path --local-install-root`/lib/libpghaskell.so . || exit 1
-echo "Done"
+#ln -s `stack path --local-install-root`/lib/libpghaskell.so . || exit 1
 
+echo "Buiding test version with cabal"
+for d in pghaskell-internal pghaskell; do
+    cd $d
+    cabal install -fdebug --force-reinstalls
+    rm -r dist
+    cd ..
+done
+ln -s ~/.cabal/lib/libpghaskell.so .
+echo "Done"
